ea1578ffcb8c8b8f40eb24cc9e1c70726ca12d5b
Isaac Olsson <Isaac.M.Olsson@gmail.com>
2026-07-23 21:58:09 -0600

    Added empty field error response to register

--- server/src/main/java/server/Handlers/RegisterHandler.java
+++ server/src/main/java/server/Handlers/RegisterHandler.java
@@ -10,15 +10,21 @@ import server.Services.RegisterService;
 public class RegisterHandler {
     public void tryRegister(Context registration) {
         var registerReq = new Gson().fromJson(registration.body(), UserData.class);
-        AuthData newAuth = RegisterService.register(registerReq);
-        if (newAuth == null) {
+        if (registerReq.username() == null || registerReq.password() == null || registerReq.email() == null) {
             registration.contentType("application/json");
-            registration.status(403);
-            registration.result(new Gson().toJson(new RegisterResponse(null, null, "Error: Username already taken")));
+            registration.status(400);
+            registration.result(new Gson().toJson(new RegisterResponse(null, null, "Error: Empty field")));
         } else {
-            RegisterResponse response = new RegisterResponse(registerReq.username(), newAuth.authToken(), null);
-            registration.contentType("application/json");
-            registration.result(new Gson().toJson(response));
+            AuthData newAuth = RegisterService.register(registerReq);
+            if (newAuth == null) {
+                registration.contentType("application/json");
+                registration.status(403);
+                registration.result(new Gson().toJson(new RegisterResponse(null, null, "Error: Username already taken")));
+            } else {
+                RegisterResponse response = new RegisterResponse(registerReq.username(), newAuth.authToken(), null);
+                registration.contentType("application/json");
+                registration.result(new Gson().toJson(response));
+            }
         }
     }
 }
